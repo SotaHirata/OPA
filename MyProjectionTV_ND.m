431fb97250c2d885214e7a0d1b5c6256df00d5d7
SotaHirata <souta20020112@gmail.com>
2023-11-16 01:58:30 +0900

    typo

--- MyProjectionTV_ND.m
+++ MyProjectionTV_ND.m
@@ -4,7 +4,7 @@ if (nargin < 5), weights = 1; end
 
 n_size = size(g);
 n_size_TV_end = size(n_size, 2);
-pn = zeros([n_size n_size_TV_end],'douuble','gpuArray');
+pn = zeros([n_size n_size_TV_end],'double','gpuArray');
 div_pn = zeros(n_size,'double','gpuArray');
 
 
