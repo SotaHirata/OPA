02a76975053916d716ba9212a2b55904111af0e1
SotaHirata <souta20020112@gmail.com>
2023-11-16 02:54:24 +0900

    [Fix] Support GPU calculation

--- inverse_bounded_TVSGD.m
+++ inverse_bounded_TVSGD.m
@@ -4,60 +4,81 @@ clear all;clc;
 %乱数seedの固定
 rng(0);
 
+% GPU
+GPU_num = 4;
+gpuDevice(GPU_num);
+reset(gpuDevice(GPU_num));
+executionEnvironment = 'gpu';
+gpurng(0);
+
 %パラメタ
-M = 4;     %uniformアレイの1辺の長さ
+M = 10;     %uniformアレイの1辺の長さ
 N = M^2;    %アンテナ数
 K =  N^2*4;    %照射パターン数
 maskD = N/2; %PDの受光範囲の直径
 
+%SGDの設定
+num_epoch = 100;  %エポック数
+batch_size = 2^6; %バッチサイズ
+num_itr = (ceil(K/batch_size))*num_epoch; %反復回数
+data_indice = randperm(K); %batch列を用意
+
 %複素振幅画像を生成（N×N）
-obj = MyRect(N, N/2);
+obj = gpuArray(double(MyRect(N,N/2))) ;
+%{
+img = imread('peppers.png');
+img_resized = imresize(img, [N, N]);
+img_gray = double(rgb2gray(img_resized)) ;
+obj = img_gray / max(img_gray(:));
+obj = gpuArray(double(obj.*MyRect(N, N)));
+%}
 
 %サポート
-sup = MyRect(N, N);
+sup =gpuArray(double(MyRect(N, N)));
 
 %アンテナ位置を表す行列（N×N）
-%array = MyRect(N, M); %for uniformアレイ
+%array = gpuArray(double(MyRect(N, M))); %for uniformアレイ
 %load('random_array_9');
-%array = randomarray;
-load('Costasarray_N16.mat') ;
-array = matrix;%for Costasアレイ
+%array = gpuArray(double(randomarray));
+%load('Costasarray_N101.mat') ;
+%array = gpuArray(double(matrix));%for Costasアレイ
 
 %位相シフトKパターン（N×N×K）
-phi = array.*rand(N,N,K)*2*pi; 
+phi = array.*rand(N,N,K,'double','gpuArray')*2*pi; 
 
 %アンテナ配置×位相シフト（N×N×K）
-A = exp(1i*phi).*array; 
+A = array.*gpuArray(double(exp(1i*phi))); 
 
 %位相バイアス（N×N）
-r = array.*rand(N)*2*pi; 
+r = array.*rand(N,N,'double','gpuArray')*2*pi; 
 
 %PDの受光範囲マスクを作成（N×N）
-mask = MyCirc(N, maskD); 
+mask = gpuArray(double(MyCirc(N, maskD))); 
 
 %PDの観測強度（K×1配列）
-S = reshape(sum(abs(MyIFFT2(MyFFT2(A.*exp(1i*r)).*obj.*sup)).^2.*mask, [1,2]), [K,1]); 
+S = zeros(1,K,'double','gpuArray');
+for batch_start = 1:batch_size:K
+    S(batch_start:min(batch_start+batch_size -1, K)) = sum(abs(MyIFFT2(MyFFT2(A(batch_start:min(batch_start+batch_size -1, K)).*exp(1i*r)).*obj.*sup)).^2.*mask, [1,2]); 
+end
+S = reshape(S, [K,1]); 
 
 
 %ここから逆問題
 figure(100);
-O_hat = ones(N); %Oの初期値（N×N）
-r_hat = array.*rand(N)*2*pi; %rの初期値（N×N）
-num_itr = 3000; %反復回数
-batch_size = 2^6; %バッチサイズ
-%es = zeros(num_itr,1);
-batch_es = zeros(num_itr,1);
+O_hat = ones(N,'double','gpuArray'); %Oの初期値（N×N）
+r_hat = array.*rand(N,'double','gpuArray')*2*pi; %rの初期値（N×N）
+batch_es = zeros(num_itr,1,'double','gpuArray');
 
 %adamの初期パラメタ
-m_O = zeros(N);
-v_O = zeros(N);
+m_O = zeros(N,'double','gpuArray');
+v_O = zeros(N,'double','gpuArray');
 alpha_O = 5e-2;
 beta_1_O = 0.95;
 beta_2_O = 0.999;
 epsilon_O = 1e-8;
 
-m_r = zeros(N);
-v_r = zeros(N);
+m_r = zeros(N,'double','gpuArray');
+v_r = zeros(N,'double','gpuArray');
 alpha_r = 5e-2;
 beta_1_r = 0.95;
 beta_2_r = 0.999;
@@ -73,82 +94,88 @@ tv_th = 1e-2;
 tv_tau = 0.05;
 tv_iter = 4; %TVの反復数
 
-v_TV_O =  ones(N);
-u_TV_O = zeros(N);
+v_TV_O =  ones(N,'double','gpuArray');
+u_TV_O = zeros(N,'double','gpuArray');
 
 %経過時間計算用
+elapsed_times = zeros(floor(num_itr/100), 1);
+itr = 0;
 hundreds = 0;
-elapsed_times = zeros(num_itr/100, 1);
 
 tic;
-for itr = 1:num_itr
+for epoch = 1:num_epoch
+    for batch_start = 1:batch_size:K
+        itr = itr + 1;
 
-    random_idx = randperm(K, batch_size);
-    batch_A = A(:,:,random_idx);
-    batch_F = MyFFT2(batch_A.*exp(1i*r_hat));
-    batch_I = MyIFFT2(batch_F.*O_hat); 
-    batch_S_hat = reshape(sum(abs(batch_I).^2.*mask, [1,2]), [batch_size,1]); 
-    batch_e = batch_S_hat - S(random_idx);
-    batch_es(itr) = mean(abs(batch_e).^2, 'all');
-    
-    %O,rの勾配
-    st_O = 2*sum(2*conj(batch_F).*MyFFT2(batch_I.*mask.*reshape(batch_e,[1,1,batch_size])),3) + 2.*rho_O.*(O_hat - (v_TV_O - u_TV_O));
-    st_r = 2*(-1i*exp(-1i*r_hat)).*sum(2*conj(batch_A).*MyIFFT2(conj(O_hat).*MyFFT2(batch_I.*mask.*reshape(batch_e,[1,1,batch_size]))),3);
-    
-    %Adam
-    [st_O, m_O, v_O] = Adam_func(st_O,m_O,v_O,itr,alpha_O,beta_1_O,beta_2_O,epsilon_O);
-    [st_r, m_r, v_r] = Adam_func(st_r,m_r,v_r,itr,alpha_r,beta_1_r,beta_2_r,epsilon_r);
-    
-    %O,rの更新
-    O_hat = (O_hat - st_O).*sup; %Oの更新式
-    r_hat = r_hat - real(st_r) ; %rの更新式
-
-    %v,uの更新
-    v_TV_O_re = reshape(MyTVpsi_ND(real(O_hat + u_TV_O), tv_th, tv_tau, tv_iter, [N, N]), [N, N]);
-    v_TV_O_im = reshape(MyTVpsi_ND(imag(O_hat + u_TV_O), tv_th, tv_tau, tv_iter, [N, N]), [N, N]);
-    v_TV_O = v_TV_O_re + 1i .*v_TV_O_im;
-    u_TV_O = u_TV_O + (O_hat - v_TV_O);
-
-    if rem(itr, 100)==0    %描画
-        hundreds = hundreds + 1;
-
-        subplot(3,3,1)
-        imagesc(abs(O_hat)); colormap gray; axis image; colorbar;
-        title(['Reconstructed amplitude ( itr=',num2str(itr), ' )']);
-
-        subplot(3,3,2)
-        imagesc(angle(O_hat)); colormap gray; axis image; colorbar;
-        title(['Reconstructed phase  ( itr=',num2str(itr), ' )']);
-
-        subplot(3,3,3)
-        imagesc(r_hat); colormap gray; axis image; colorbar;
-        title(['Reconstructed phase bias ( itr=',num2str(itr), ' )']);
-
-        subplot(3,3,4)
-        imagesc(abs(obj)); colormap gray; axis image; colorbar;
-        title(['Original object amplitude ( itr=',num2str(itr), ' )']);
-
-        subplot(3,3,5)
-        imagesc(angle(obj)); colormap gray; axis image; colorbar;
-        title(['Original object phase  ( itr=',num2str(itr), ' )']);
-
-        subplot(3,3,6)
-        imagesc(r); colormap gray; axis image; colorbar;
-        title(['Original phase bias ( itr=',num2str(itr), ' )']);
-
-        subplot(3,3,[7,8,9])
-        semilogy(batch_es(1:itr));
-        title(['|S_{hat} - S|^2 (', num2str(sum(elapsed_times)/hundreds,4),'sec/100itr) ','(batchsize=',num2str(batch_size),')']);
-        drawnow();
+        batch_idx = data_indice(batch_start:min(batch_start+batch_size-1, K));
+        batch_A = A(:,:,batch_idx);
+        batch_F = MyFFT2(batch_A.*exp(1i*r_hat));
+        batch_I = MyIFFT2(batch_F.*O_hat); 
+        batch_S_hat = reshape(sum(abs(batch_I).^2.*mask, [1,2]),[length(batch_idx),1]); 
+        batch_e = batch_S_hat - S(batch_idx);
+        batch_es(itr) = mean(abs(batch_e).^2, 'all');
         
-        %イテレーションごとの所要時間
-        elapsed_time = toc;
-        fprintf('イテレーション %d の経過時間: %f 秒\n', itr, elapsed_time);
-        elapsed_times(hundreds) = elapsed_time;
-        if itr ~=num_itr
-           tic;
-        end 
+        %O,rの勾配
+        st_O = 2*sum(2*conj(batch_F).*MyFFT2(batch_I.*mask.*reshape(batch_e,[1,1,length(batch_idx)])),3) + 2.*rho_O.*(O_hat - (v_TV_O - u_TV_O));
+        st_r = 2*(-1i*exp(-1i*r_hat)).*sum(2*conj(batch_A).*MyIFFT2(conj(O_hat).*MyFFT2(batch_I.*mask.*reshape(batch_e,[1,1,length(batch_idx)]))),3);
+        
+        %Adam
+        [st_O, m_O, v_O] = Adam_func(st_O,m_O,v_O,itr,alpha_O,beta_1_O,beta_2_O,epsilon_O);
+        [st_r, m_r, v_r] = Adam_func(st_r,m_r,v_r,itr,alpha_r,beta_1_r,beta_2_r,epsilon_r);
+        
+        %O,rの更新
+        O_hat = (O_hat - st_O).*sup; %Oの更新式
+        r_hat = r_hat - real(st_r) ; %rの更新式
+    
+        %v,uの更新
+        v_TV_O_re = reshape(MyTVpsi_ND(real(O_hat + u_TV_O), tv_th, tv_tau, tv_iter, [N, N]), [N, N]);
+        v_TV_O_im = reshape(MyTVpsi_ND(imag(O_hat + u_TV_O), tv_th, tv_tau, tv_iter, [N, N]), [N, N]);
+        v_TV_O = v_TV_O_re + 1i .*v_TV_O_im;
+        u_TV_O = u_TV_O + (O_hat - v_TV_O);
+    
+        if rem(itr, 100)==0    %描画
+            hundreds = hundreds + 1;
+    
+            subplot(3,3,1)
+            imagesc(abs(O_hat)); colormap gray; axis image; colorbar;
+            title(['Reconstructed amplitude ( itr=',num2str(itr), ' )']);
+    
+            subplot(3,3,2)
+            imagesc(angle(O_hat)); colormap gray; axis image; colorbar;
+            title(['Reconstructed phase  ( itr=',num2str(itr), ' )']);
+    
+            subplot(3,3,3)
+            imagesc(r_hat); colormap gray; axis image; colorbar;
+            title(['Reconstructed phase bias ( itr=',num2str(itr), ' )']);
+    
+            subplot(3,3,4)
+            imagesc(abs(obj)); colormap gray; axis image; colorbar;
+            title(['Original object amplitude ( itr=',num2str(itr), ' )']);
+    
+            subplot(3,3,5)
+            imagesc(angle(obj)); colormap gray; axis image; colorbar;
+            title(['Original object phase  ( itr=',num2str(itr), ' )']);
+    
+            subplot(3,3,6)
+            imagesc(r); colormap gray; axis image; colorbar;
+            title(['Original phase bias ( itr=',num2str(itr), ' )']);
+    
+            subplot(3,3,[7,8,9])
+            semilogy(batch_es(1:itr));
+            title(['|S_{hat} - S|^2 (', num2str(sum(elapsed_times)/hundreds,4),'sec/100itr) ','(batchsize=',num2str(batch_size),')']);
+            drawnow();
+            
+            %イテレーションごとの所要時間
+            elapsed_time = toc;
+            fprintf('イテレーション %d の経過時間: %f 秒\n', itr, elapsed_time);
+            elapsed_times(hundreds) = elapsed_time;
+            if itr ~=num_itr
+               tic;
+            end
+        end
     end
 end
+
+    
 
 
